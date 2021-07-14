965cbf8a79d0075cd63236d1d1c67771ada595f3
Timothy Fox <fox5681@students.cptc.edu>
2021-07-13 22:08:42 -0700

    changed welcome msg grammer added user input to continue

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace GitRepo071221
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello and welcome to the middle of this film!");
+            Console.WriteLine("Hello and welcome to the middle of the film!");
+            Console.WriteLine("Should you wish to continue, press the any key.");
+
+            Console.ReadKey();
+           
         }
     }
 }
