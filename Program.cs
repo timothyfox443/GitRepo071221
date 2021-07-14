5c2b3b82031def056b2a2d14d59271eb0116ac6e
Timothy Fox <fox5681@students.cptc.edu>
2021-07-13 21:50:33 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitRepo071221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
